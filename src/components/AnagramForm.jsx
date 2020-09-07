5e43d346a3e36bc968e4e0dc751415b6f010aede
Joe <josephnewton861@yahoo.co.uk>
2020-09-07 09:01:09 +0100

    Added component for anagrams

---
+++ src/components/AnagramForm.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AnagramForm = () => {
+  return (
+    <div>
+      <input type="text" />
+      <input type="text" />
+      <button>Log Anagrams</button>
+    </div>
+  );
+};
+
+export default AnagramForm;
