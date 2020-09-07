b2135926244a34f7684dbd67cb8cc6eb78d3a715
Joe <josephnewton861@yahoo.co.uk>
2020-09-07 09:05:45 +0100

    Added functionality for detecting if input is a palindrome

--- src/components/PalindromeForm.jsx
+++ src/components/PalindromeForm.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PalindromeForm = () => {
+  const [palindromeInput, setPalindromeInput] = useState([]);
+
+  const isPalindrome = (str) => {
+    if (typeof str !== "string") return "Input requires a string";
+
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] !== str[str.length - 1 - i]) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const handlesPalindromeChange = (event) => {
+    setPalindromeInput(event.target.value);
+  };
+
   return (
     <div>
-      <input type="text" />
+      <input type="text" onChange={handlesPalindromeChange} />
       <button>Log Palindrome</button>
+      {isPalindrome(palindromeInput) === false ? <p>False</p> : <p>True</p>}
     </div>
   );
 };
