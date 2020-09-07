aaeeac4c09561c4b1ff99ade0e9ffa210b5702d8
Joe <josephnewton861@yahoo.co.uk>
2020-09-07 08:58:55 +0100

    Added component for palindromes

---
+++ src/components/PalindromeForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PalindromeForm = () => {
+  return (
+    <div>
+      <input type="text" />
+      <button>Log Palindrome</button>
+    </div>
+  );
+};
+
+export default PalindromeForm;
